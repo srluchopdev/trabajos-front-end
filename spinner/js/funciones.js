7312cbf07fb8ce09c85db74816f54c1587f073e1
lucianonhperiera <lucianonhpereira@gmail.com>
2024-08-25 18:35:20 -0300

    .

--- spinner/js/funciones.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('startButton').addEventListener('click', function() {
-    document.getElementById('spinner').classList.add('active');
-});
-
-document.getElementById('stopButton').addEventListener('click', function() {
-    document.getElementById('spinner').classList.remove('active');
-});
